0b4bac865efae1be697cfa0da5d9b13ff58d67af
Nayaabkhan Khan <nayaab@hey.com>
2023-08-30 12:15:14 +0530

    Add more quote and randomize

--- quotes.js
+++ quotes.js
@@ -1,7 +1,31 @@
 const quotes = [
   {
-    "body": "To dare is to lose one’s footing momentarily. Not to dare is to lose oneself.",
-    "author": "Søren Kierkegaard"
+    body: "To dare is to lose one’s footing momentarily. Not to dare is to lose oneself.",
+    author: "Søren Kierkegaard"
+  },
+  {
+    body: "Sometimes even to live is an act of courage.",
+    author: "Lucius Annaeus Seneca"
+  },
+  {
+    body: "We suffer more often in imagination than in reality",
+    author: "Lucius Annaeus Seneca"
+  },
+  {
+    body: "It is not the man who has too little, but the man who craves more, that is poor.",
+    author: "Lucius Annaeus Seneca"
+  },
+  {
+    body: "It is better to conquer our grief than to deceive it.",
+    author: "Lucius Annaeus Seneca"
+  },
+  {
+    body: "No man is more unhappy than he who never faces adversity. For he is not permitted to prove himself.",
+    author: "Lucius Annaeus Seneca"
+  },
+  {
+    body: "All cruelty springs from weakness.",
+    author: "Lucius Annaeus Seneca"
   }
 ]
 
@@ -9,6 +33,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const bodyElement = document.getElementById('cd-quote-body');
   const authorElement = document.getElementById('cd-quote-author');
 
-  bodyElement.innerText = quotes[0].body;
-  authorElement.innerText = quotes[0].author ?? 'Anonymous';
+  const quote = quotes[Math.floor(Math.random() * quotes.length)];
+
+  bodyElement.innerText = quote.body;
+  authorElement.innerText = quote.author ?? 'Anonymous';
 });
