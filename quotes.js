3c7bfee382d232b44e4548c2aa92cb51ac3ac0da
Nayaabkhan Khan <nayaab@hey.com>
2023-08-30 11:59:29 +0530

    Show author

--- quotes.js
+++ quotes.js
@@ -1,11 +1,14 @@
 const quotes = [
   {
-    "body": "To dare is to lose one’s footing momentarily. Not to dare is to lose oneself."
+    "body": "To dare is to lose one’s footing momentarily. Not to dare is to lose oneself.",
+    "author": "Søren Kierkegaard"
   }
 ]
 
 document.addEventListener('DOMContentLoaded', () => {
-  const quoteElement = document.getElementById('cd-quote');
+  const bodyElement = document.getElementById('cd-quote-body');
+  const authorElement = document.getElementById('cd-quote-author');
 
-  quoteElement.innerText = quotes[0].body;
+  bodyElement.innerText = quotes[0].body;
+  authorElement.innerText = quotes[0].author ?? 'Anonymous';
 });
